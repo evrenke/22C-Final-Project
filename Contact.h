f3ebc603d748ee31d8f6fc1997fc8679b3a018a9
evrenke <evrenkeskin300@gmail.com>
2018-11-29 14:19:40 -0800

    Update Contact.h

--- Contact.h
+++ Contact.h
@@ -1,14 +1,29 @@
+#pragma once
 #include <string>
-
+#include "Location.h"
 class Contact {
-	private:
-		//Location location;
-		std::string name;
-		std::string address;
-		int contactNum;
-	public:
-		std::string getName() { return name; }
-		std::string getAdress() { return address;}
-		int getContactNum() { return contactNum; }
-		//location getLocation(){ return location; }
+private:
+	Location location;
+	std::string name;
+	std::string address;
+	int contactNum;
+public:
+	Contact()
+	{
+		name = "No man";
+		address = "Middle of Nowhere";
+		contactNum = 5555555555;
+		location = Location();
+	}
+	Contact(std::string n, std::string a, int num, Location l)
+	{
+		name = n;
+		address = a;
+		contactNum = num;
+		location = l;
+	}
+	std::string getName() { return name; }
+	std::string getAdress() { return address; }
+	int getContactNum() { return contactNum; }
+	Location getLocation(){ return location; }
 };
