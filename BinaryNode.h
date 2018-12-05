7252ba80dcd974b9d688a3c854ed84ad0da9db43
iceicebaebee <44150243+iceicebaebee@users.noreply.github.com>
2018-12-05 15:19:26 -0800

    Add files via upload

--- BinaryNode.h
+++ BinaryNode.h
@@ -1,11 +1,22 @@
+//
+//  Location.h
+//  22C_Project
+//
+//  Created by Ohreum, Tommaso, Evren, Jason on 11/16/18.
+
 #pragma once
 #include "Node.h"
+/*
+ This class has two private pointers: left and right child.
+ It also has getter and setter methods for the pointers and information
+ */
 template<class ot>
 class BinaryNode : public Node<ot> {
 private:
 	BinaryNode<ot>* rightChild;
 	BinaryNode<ot>* leftChild;
 public:
+    //Default constructor
 	BinaryNode() {
 		//setData(nullptr);
 		setLeftChild(nullptr);
@@ -14,12 +25,17 @@ public:
 	BinaryNode(ot object) {
 		this->setData(object);
 	}
-
+    //returns left child
 	BinaryNode<ot>* getLeftChild() { return leftChild; }
+    //sets left child
 	void setLeftChild(BinaryNode<ot>* in) { leftChild = in; }
+    //returns right child
 	BinaryNode<ot>* getRightChild() { return rightChild; }
+    //sets right child
 	void setRightChild(BinaryNode<ot>* in) { rightChild = in; }
+    //returns data inside
 	const ot getInfo() { return this->getData(); }
+    //sets data inside
 	void setInfo(ot in) { this->setData(in); }
 
 	//Deletes all nodes below to prevent memory leaks. Deleting the root will delete the entire tree.
